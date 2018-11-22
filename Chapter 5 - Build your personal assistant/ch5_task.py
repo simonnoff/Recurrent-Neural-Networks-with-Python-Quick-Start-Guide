46dc9df0b520f58e21156aa6e7f9a0e86e808d76
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2018-11-22 19:45:39 +0000

    Update chapter 5 to distinguish tensorFlow from tensorLayer

--- Chapter 5 - Build your personal assistant/ch5_task.py
+++ Chapter 5 - Build your personal assistant/ch5_task.py
@@ -1,6 +1,6 @@
 import time
-import tensorflow as tf
-import tensorlayer as tl
+import tensorflow as tflow
+import tensorlayer as tlayer
 from sklearn.utils import shuffle
 from tensorlayer.layers import EmbeddingInputlayer, Seq2Seq, DenseLayer, retrieve_seq_length_op2
 
@@ -15,12 +15,12 @@ testY = testY.tolist()
 validX = validX.tolist()
 validY = validY.tolist()
 
-trainX = tl.prepro.remove_pad_sequences(trainX)
-trainY = tl.prepro.remove_pad_sequences(trainY)
-testX = tl.prepro.remove_pad_sequences(testX)
-testY = tl.prepro.remove_pad_sequences(testY)
-validX = tl.prepro.remove_pad_sequences(validX)
-validY = tl.prepro.remove_pad_sequences(validY)
+trainX = tlayer.prepro.remove_pad_sequences(trainX)
+trainY = tlayer.prepro.remove_pad_sequences(trainY)
+testX = tlayer.prepro.remove_pad_sequences(testX)
+testY = tlayer.prepro.remove_pad_sequences(testY)
+validX = tlayer.prepro.remove_pad_sequences(validX)
+validY = tlayer.prepro.remove_pad_sequences(validY)
 
 # Hyperparameters
 batch_size = 32
@@ -49,8 +49,8 @@ xvocab_size = yvocab_size = xvocab_size + 2
 
 # Model
 def model(encode_seqs, decode_seqs, is_train=True, reuse=False):
-    with tf.variable_scope("model", reuse=reuse):
-        with tf.variable_scope("embedding") as vs:
+    with tflow.variable_scope("model", reuse=reuse):
+        with tflow.variable_scope("embedding") as vs:
             net_encode = EmbeddingInputlayer(
                 inputs = encode_seqs,
                 vocabulary_size = xvocab_size,
@@ -63,34 +63,34 @@ def model(encode_seqs, decode_seqs, is_train=True, reuse=False):
                 embedding_size = embedding_dimension,
                 name = 'seq_embedding')
         net_rnn = Seq2Seq(net_encode, net_decode,
-                cell_fn = tf.contrib.rnn.BasicLSTMCell,
+                cell_fn = tflow.contrib.rnn.BasicLSTMCell,
                 n_hidden = embedding_dimension,
-                initializer = tf.random_uniform_initializer(-0.1, 0.1),
+                initializer = tflow.random_uniform_initializer(-0.1, 0.1),
                 encode_sequence_length = retrieve_seq_length_op2(encode_seqs),
                 decode_sequence_length = retrieve_seq_length_op2(decode_seqs),
                 initial_state_encode = None,
                 n_layer = 3,
                 return_seq_2d = True,
                 name = 'seq2seq')
-        net_out = DenseLayer(net_rnn, n_units=xvocab_size, act=tf.identity, name='output')
+        net_out = DenseLayer(net_rnn, n_units=xvocab_size, act=tflow.identity, name='output')
     return net_out, net_rnn
 
 # Initialize model for training
-encode_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="encode_seqs")
-decode_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="decode_seqs")
-target_seqs = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="target_seqs")
-target_mask = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name="target_mask")
+encode_seqs = tflow.placeholder(dtype=tflow.int64, shape=[batch_size, None], name="encode_seqs")
+decode_seqs = tflow.placeholder(dtype=tflow.int64, shape=[batch_size, None], name="decode_seqs")
+target_seqs = tflow.placeholder(dtype=tflow.int64, shape=[batch_size, None], name="target_seqs")
+target_mask = tflow.placeholder(dtype=tflow.int64, shape=[batch_size, None], name="target_mask")
 net_out, _ = model(encode_seqs, decode_seqs, is_train=True, reuse=False)
 
-loss = tl.cost.cross_entropy_seq_with_mask(logits=net_out.outputs, target_seqs=target_seqs, input_mask=target_mask, name='cost')
+loss = tlayer.cost.cross_entropy_seq_with_mask(logits=net_out.outputs, target_seqs=target_seqs, input_mask=target_mask, name='cost')
 
 #net_out.print_params(False)
 
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+optimizer = tflow.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
-# Understand tf.ConfigProto - https://stackoverflow.com/questions/44873273/what-do-the-options-in-configproto-like-allow-soft-placement-and-log-device-plac
-sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False))
-sess.run(tf.global_variables_initializer())
+# Understand tflow.ConfigProto - https://stackoverflow.com/questions/44873273/what-do-the-options-in-configproto-like-allow-soft-placement-and-log-device-plac
+sess = tflow.Session(config=tflow.ConfigProto(allow_soft_placement=True, log_device_placement=False))
+sess.run(tflow.global_variables_initializer())
 
 # Train
 def train():
@@ -99,16 +99,16 @@ def train():
         epoch_time = time.time()
         trainX_shuffled, trainY_shuffled = shuffle(trainX, trainY, random_state=0)
         total_err, n_iter = 0, 0
-        for X, Y in tl.iterate.minibatches(inputs=trainX_shuffled, targets=trainY_shuffled, batch_size=batch_size, shuffle=False):
+        for X, Y in tlayer.iterate.minibatches(inputs=trainX_shuffled, targets=trainY_shuffled, batch_size=batch_size, shuffle=False):
 
-            X = tl.prepro.pad_sequences(X)
+            X = tlayer.prepro.pad_sequences(X)
 
-            _decode_seqs = tl.prepro.sequences_add_start_id(Y, start_id=start_id, remove_last=False)
-            _decode_seqs = tl.prepro.pad_sequences(_decode_seqs)
+            _decode_seqs = tlayer.prepro.sequences_add_start_id(Y, start_id=start_id, remove_last=False)
+            _decode_seqs = tlayer.prepro.pad_sequences(_decode_seqs)
 
-            _target_seqs = tl.prepro.sequences_add_end_id(Y, end_id=end_id)
-            _target_seqs = tl.prepro.pad_sequences(_target_seqs)
-            _target_mask = tl.prepro.sequences_get_mask(_target_seqs)
+            _target_seqs = tlayer.prepro.sequences_add_end_id(Y, end_id=end_id)
+            _target_seqs = tlayer.prepro.pad_sequences(_target_seqs)
+            _target_mask = tlayer.prepro.sequences_get_mask(_target_seqs)
 
             _, err = sess.run([optimizer, loss],
                             {encode_seqs: X,
@@ -122,10 +122,10 @@ def train():
             total_err += err; n_iter += 1
 
 # Initialize model for prediction
-encode_seqs2 = tf.placeholder(dtype=tf.int64, shape=[1, None], name="encode_seqs")
-decode_seqs2 = tf.placeholder(dtype=tf.int64, shape=[1, None], name="decode_seqs")
+encode_seqs2 = tflow.placeholder(dtype=tflow.int64, shape=[1, None], name="encode_seqs")
+decode_seqs2 = tflow.placeholder(dtype=tflow.int64, shape=[1, None], name="decode_seqs")
 net, net_rnn = model(encode_seqs2, decode_seqs2, is_train=False, reuse=True)
-y = tf.nn.softmax(net.outputs)
+y = tflow.nn.softmax(net.outputs)
 
 # Predict
 def predict():
@@ -141,7 +141,7 @@ def predict():
             o, state = sess.run([y, net_rnn.final_state_decode],
                             {net_rnn.initial_state_decode: state,
                             decode_seqs2: [[start_id]]})
-            w_id = tl.nlp.sample_top(o[0], top_k=3)
+            w_id = tlayer.nlp.sample_top(o[0], top_k=3)
             w = idx2w[w_id]
             # 3. decode, feed state iteratively
             sentence = [w]
@@ -149,7 +149,7 @@ def predict():
                 o, state = sess.run([y, net_rnn.final_state_decode],
                                 {net_rnn.initial_state_decode: state,
                                 decode_seqs2: [[w_id]]})
-                w_id = tl.nlp.sample_top(o[0], top_k=2)
+                w_id = tlayer.nlp.sample_top(o[0], top_k=2)
                 w = idx2w[w_id]
                 if w_id == end_id:
                     break
